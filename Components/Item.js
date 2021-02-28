fef67af2696db85b47e7d3fd1a8a2547a4ab3fe9
Jami Hannus <t9haja00@student.oamk.fi>
2021-02-28 12:17:49 +0200

    Item mapping Search funtion by location and category

--- Components/Item.js
+++ Components/Item.js
@@ -1,30 +1,50 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { Card, Button,Viev,Image } from 'react-native-elements';
+import { Text, StyleSheet ,Image} from 'react-native';
+import { Card, Button,Viev} from 'react-native-elements';
 
 
-export default function Item(Item) {
-    console.log(Item);
-       return(
+export default function Item(props) {
+        {console.log(props)};
+       let Item =props;
+    if (Item.hasOwnProperty('iditem')){
+                        return(                                              
+                                                <Card style={styles.card} key={Item.iditem}>
+                                                <Card.Title>{Item.title}</Card.Title>
+                                                <Text>{Item.description}</Text>
+                                                <Text>Price {Item.price}</Text>
+                                                <Text>Location {Item.location}</Text>
+                                                <Text>Category {Item.category}</Text>
+                                                <Text>Delivery {Item.deliverytype}</Text>
+                                                <Text>Posted  {Item.dateposted.split('T')[0]}</Text>
+                                                <Card.Image style={styles.image}  source= {{uri:Item.images[0]}}></Card.Image>
+                                                <Card.Image style={styles.image}  source= {{uri:Item.images[1]}}></Card.Image>
+                                                <Card.Image style={styles.image} source= {{uri:Item.images[2]}}></Card.Image>  
+                                                <Card.Image style={styles.image} source= {{uri:Item.images[3]}}></Card.Image>    
+                                                </Card>
+                                                )           
+                        }
+    else 
+     return(
+         <Card>
+             <Text>Nothing</Text>
+              </Card>)  
+};
        
-                    <Card key={Item.iditem}>
-                        <Text>{Item.title}</Text>
-                        <Text>{Item.description}</Text>
-                        <Text>{Item.price}</Text>
-                        <Text>{Item.location}</Text>
-                        <Text>{Item.category}</Text>
-                        <Text>{Item.deliverytype}</Text>
-                        <Text>{Item.dateposted}</Text>
-                        <Image source={{uri: Item.images[0]}} style={ { width: 100, height: 100 } }></Image>
-                    </Card>
-           
-           )
-       
-}
 
 
 const styles = StyleSheet.create({
-    name: {
+    card: {
+        height: 500,
+        marginVertical: 10,
+        flexDirection: 'row',
+        shadowColor: '#999',
+        shadowOffset: {width: 0, height: 1},
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5,
+        borderRadius: 10,
+      },
+    title: {
         color: '#5a647d',
         fontWeight: 'bold',
         fontSize: 30
@@ -36,5 +56,9 @@ const styles = StyleSheet.create({
     description: {
         fontSize: 10,
         color: '#c1c4cd'
-    }
+    },
+    image : {
+        width : '100%',
+        height : '70%'
+    },
 });
--- Screens/Items.js
+++ Screens/Items.js
@@ -1,45 +1,41 @@
 import React,{useState,useEffect} from 'react';
-import {StyleSheet,Text,Button,ScrollView,View } from 'react-native';
+import {StyleSheet,Text,Button,ScrollView,View,TextInput } from 'react-native';
 import Item from '../Components/Item';
-
-
-const products = [
-  {
-    name: 'Khaki Suede Polish Work Boots',
-    price: 149.99
-  },
-  {
-    name: 'Camo Fang Backpack Jungle',
-    price: 39.99
-  },
-  {
-    name: 'Parka and Quilted Liner Jacket',
-    price: 49.99
-  },
-  {
-    name: 'Cotton Black Cap',
-    price: 12.99
-  }
-];
-
+import {Picker} from '@react-native-picker/picker';
 export function Items({route,navigation}) {
   
-useEffect(() => {
-    ItemFetch();
+useEffect(() => { 
   }, []);
 
 const {apiURI} =route.params; 
 
 
-const [allItems, SetAllItems] = useState(
-  {test:'test'}
-);
-const [loading, SetLoading] = useState(false);
+const [allItems, SetAllItems] = useState('');
+const [hasdata, SetHasdata] = useState(false);
 const [id, Setid] = useState(73);
-  
- 
+const [category, setCategory] = useState(0);  
+const [location, setLocation] = useState('');  
+
+
     const ItemFetch= () => {
-    fetch(apiURI+'/items/'+id, {
+          if (category ==0 && location ==""){
+            //no parameters get some items
+            address=apiURI+ '/items/0'
+                }
+      else if( category == 0 && !(location == "")){
+            //location has changed
+            address=apiURI+'/items/location/'+location
+            }
+      else if (location=="" && !(category==0)){
+        //category has changed
+        address=apiURI+'/items/category/'+category
+      }
+      else{
+          //both have changed
+          address=apiURI+'/items/multi/'+location/+category
+      }
+    console.log(address);
+    fetch(address, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -48,17 +44,15 @@ const [id, Setid] = useState(73);
     .then(response => response.json())
     .then(data => {
       alert('YAY');
-      console.log(...data);
-      SetAllItems(...data)
+      SetAllItems(data);
+      SetHasdata(true);
+      console.log(hasdata);
     })
     .catch((error) => {
       alert('NAY');
       console.error('Error:', error);
     });
   }
-  
-
-
     return (
       
       <ScrollView
@@ -71,27 +65,85 @@ const [id, Setid] = useState(73);
           title="Go to login"
           onPress={() => navigation.navigate('Login Screen')}
         />
-        <View style={{ alignItems: 'center', flex: 1, marginTop: 30, flexDirection:"column"}}>   
+        <View style={{ alignItems: 'center', flex: 1, marginTop: 30, flexDirection:"column"}}>  
+        <Text>Search by category</Text>
+        <Picker 
+        style={{height: 50, width:200} }
+        selectedValue={category}
+        onValueChange={(itemValue) =>
+          setCategory(itemValue)}>
+          <Picker.Item label="All categories" value={0} />
+          <Picker.Item label="Furniture" value="Furniture" />
+          <Picker.Item label="Clothes" value="Clothes" />
+          <Picker.Item label="Car parts" value="Car parts" />
+          <Picker.Item label="Electronics" value="Electronics" />
+          <Picker.Item label="Other" value="Other" />
+          </Picker>
+          <Text>Search by location</Text>
+          <TextInput
+        style={ styles.input }
+        value={ location }
+        placeholder="Can be empty"
+        onChangeText={ value => setLocation(value)}
+      />
     <Button    
-       title="More Items"
+       title="Get some Items"
        onPress={() => ItemFetch()}
      />
-     <Button    
-       title="More loading"
-       onPress={() => SetLoading(true)}
-     />
+     { hasdata && allItems.map(item =><Item {...item}></Item>)}
       </View>
-      { loading && <Item {...allItems}></Item>}
       </ScrollView>
     )
   };
 const styles = StyleSheet.create({
-  row: {
+  screen: {
+    backgroundColor: 'rgb(51, 153, 255)',
     flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
-      },
-  col: {
-    flex: 1,
-      }
+    alignItems: 'center'
+  },
+  header: {
+    fontSize: 40,
+    marginBottom: 20,
+    color: 'white'
+  },
+  text: {
+    fontSize: 20,
+    color: 'black'
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 20,
+    height: 40,
+    width: '90%',
+    backgroundColor: 'white',
+    textAlign: 'center',
+    fontSize: 18,
+    marginTop: 5,
+    marginBottom: 20
+  },
+  primaryButton: {
+    backgroundColor: 'rgb(0, 153, 51)',
+    height: 60,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 2,
+    marginTop: 20,
+    marginBottom: 10
+  },
+  primaryButtonText: {
+    color: 'white',
+    fontSize: 20
+  },
+  scrollView: {
+    marginHorizontal: 1,
+  },
+  view:{
+    borderWidth: 1,
+    borderRadius: 4 ,
+    marginTop: 20,
+    marginBottom: 10
+  }
     })
